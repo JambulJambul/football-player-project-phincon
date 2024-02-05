9f07d2c1b747d05681b478f63db1039c56d26bf1
JambulJambul <hilman_fahmi21@yahoo.com>
2024-02-05 11:39:03 +0700

    change default theme to light

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -8,56 +8,20 @@ import encryptPayload from '@utils/encryptionHelper';
 
 import { selectPokemon } from './selectors';
 
-const Home = ({ pokemon }) => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
-  const [user, setUser] = useState({});
-  useEffect(() => {
-    dispatch(fetchPokemon());
-  }, [dispatch]);
-
-  const onChangeHandler = (value, type) => {
-    setUser({
-      ...user,
-      [type]: value
-    })
-  }
-
-  const onSubmit = () => {
-    const dataUser = {
-      fullname: encryptPayload(user.fullname),
-      email: encryptPayload(user.email),
-      password: encryptPayload(user.password),
-    }
-    dispatch(register(
-      dataUser,
-      () => {
-        console.log("Callback success");
-        navigate('/login');
-      },
-      (error) => {
-        console.log(error);
-      }
-    ))
-  }
-
+const Home = () => {
   return (
-    <div>
-      <input onChange={(e) => onChangeHandler(e.target.value, 'fullname')} placeholder='fullname' />
-      <input onChange={(e) => onChangeHandler(e.target.value, 'email')} placeholder='email' />
-      <input onChange={(e) => onChangeHandler(e.target.value, 'password')} placeholder='password' />
-      <button onClick={() => onSubmit()}>Submit</button>
-    </div>
+    <>
+      <div>
+        TEST ANJAY
+      </div>
+    </>
   );
 };
 
 Home.propTypes = {
-  pokemon: PropTypes.object,
 };
 
 const mapStateToProps = createStructuredSelector({
-  pokemon: selectPokemon
 });
 
 export default connect(mapStateToProps)(Home);
